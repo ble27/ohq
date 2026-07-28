10592d1961160ddd24a0af17c6afb18896c9746c
ble27 <bryantle2706@outlook.com>
2026-07-27 23:59:00 -0500

    Temporary placeholder space header

--- my-app/src/components/Body.tsx
+++ my-app/src/components/Body.tsx
@@ -5,14 +5,14 @@ export const Body = () => {
     <div className="flex flex-col items-center w-full gap-4 mt-0 pt-0">
         <p className="pt-10 flex flex-col justify-center items-center w-full h-48 text-xl sm:h-20 sm:text-2xl font-semibold text-center mb-10">
             Select your class and receive live updates for office hours seamlessly.
-            <span className="mt-5 justify-center items-center w-full h-48 text-xs sm:h-20 sm:text-xl font-light text-center"> 
+            <span className="mt-5 justify-center items-center w-full h-48 md:text-lg sm:h-20 sm:text-xl font-light text-center"> 
                 Alert when a TA/PT is available for help 
             </span>
-            <span className="justify-center items-center w-full h-48 text-xs sm:h-20 sm:text-xl font-light text-center"> 
+            <span className="justify-center items-center w-full h-48 md:text-lg sm:h-20 sm:text-xl font-light text-center"> 
                 Remove stress about waiting
             </span>
         </p>
-        <button className='w-30 font-inter bg-black text-white p-3 rounded-lg text-center'>
+        <button className='w-1/3 font-inter bg-black text-white p-3 rounded-full text-center'>
             Join Now
         </button>
     </div>
--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 export const Header = () => {
   return (
     <>
-    <nav className='flex flex-row justify-between items-center sticky top-0 z-50 w-full pl-5 pt-5 pr-10 pb-10 bg-white'>
-        <a href="#" className='font-sans text-3xl font-bold'>
-            Queue
-        </a>
-        <a href="#" className='font-sans text-xl sm:text-lg text-gray-500 focus:opacity-90'>
-            docs
-        </a>
-        <div className="w-[105px] hidden sm:block"></div> 
+   <nav className='flex flex-row items-center sticky top-0 z-50 h-20 pl-5 pr-10 bg-white mb-20'>
+    <a href="#" className='w-1/3 font-sans text-2xl font-bold bg-blue-500'> Queue </a> 
+    <div className='flex justify-center items-center gap-10 h-full w-1/3 bg-red-500'> 
+        <a href="https://github.com/ble27" className='font-sans text-xl sm:text-xs text-gray-500 focus:opacity-90'> docs </a> 
+        <a href="https://github.com/ble27" className='font-sans text-xl sm:text-xs text-gray-500 focus:opacity-90'> source </a> 
+    </div> 
+    <div className='flex justify-end w-1/3 bg-yellow-500'>Ye</div>
+    {/* <div className="w-[105px] hidden sm:block"></div>  */}
     </nav>
     </>    
   )
